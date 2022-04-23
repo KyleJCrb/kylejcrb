1680cfd37031bba85e5ef30e2fa3c4134e6dbcb4
KyleJCrb <59068189+KyleJCrb@users.noreply.github.com>
2022-04-22 23:29:38 -0700

    Update deemon.sh

--- deemon.sh
+++ deemon.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-apk add gcc &&
-apk add py3-pycryptodomex &&
-apk add py3-pip &&
-pip install packaging -I &&
-pip install deemon
-#
-# deemon default configuration location is /root/.config/deemon
+apk add gcc &&  # pycryptodomex dependency
+apk add py3-pycryptodomex &&  # deemon installation dependency
+apk add py3-pip &&  # python3 pip install
+pip install packaging -I &&  # packaging application included with py3-pip is outdated and will cause failures, this replaces it with the latest
+pip install deemon  # deemon default configuration location is /root/.config/deemon
